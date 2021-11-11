359dc16c9d2a329bbca49be587b692d358cb7c49
Wolthrax <wolthrax@gmail.com>
2021-11-11 15:28:06 +0100

    Project structure

---
+++ src/main/java/com/mapofzones/tokenmatcher/TokenMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.mapofzones.tokenmatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenMatcherApplication.class, args);
+	}
+
+}
