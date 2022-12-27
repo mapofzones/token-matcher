856b7b88108e0d988bbeb1231c465f4b0ef47b87
Wolthrax <wolthrax@gmail.com>
2022-12-27 11:03:25 +0300

    fix tests

--- src/test/java/com/mapofzones/tokenmatcher/service/denomtraces/client/DenomTraceClientTest.java
+++ src/test/java/com/mapofzones/tokenmatcher/service/denomtraces/client/DenomTraceClientTest.java
@@ -5,7 +5,6 @@ import com.mapofzones.tokenmatcher.AbstractTest;
 import com.mapofzones.tokenmatcher.config.DenomTraceClientConfig;
 import com.mapofzones.tokenmatcher.config.PropertiesConfig;
 import com.mapofzones.tokenmatcher.config.TestConfig;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
@@ -18,29 +17,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ContextConfiguration(classes = {DenomTraceClientConfig.class, TestConfig.class, PropertiesConfig.class})
 public class DenomTraceClientTest extends AbstractTest {
 
-    private static final String COSMOSHUB_LCD = "http://159.203.104.207:1317";
-    private static final String OSMOSIS_LCD = "http://35.235.89.254:1317";
-    private static final String AKASHNET_LCD = "http://3.235.249.94:1317";
+    private static final String COSMOSHUB_LCD = "http://207.244.247.175:1317";
+    private static final String OSMOSIS_LCD = "http://34.141.36.207:1317";
+    private static final String AKASHNET_LCD = "http://104.218.18.182:1317";
 
     @Autowired
     private DenomTraceClient denomTraceClient;
 
-    @Disabled
     @Test
     public void findDenomTrace_IsSuccessfulReceived_Test() {
-        DenomTraceDto receivedDto1 = denomTraceClient.findDenomTrace(COSMOSHUB_LCD, "14F9BC3E44B8A9C1BE1FB08980FAB87034C9905EF17CF2F5008FC085218811CC");
-        DenomTraceDto receivedDto2 = denomTraceClient.findDenomTrace(OSMOSIS_LCD, "1DCC8A6CB5689018431323953344A9F6CC4D0BFB261E88C9F7777372C10CD076");
-        DenomTraceDto receivedDto3 = denomTraceClient.findDenomTrace(AKASHNET_LCD, "27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2");
-
+        DenomTraceDto receivedDto1 = denomTraceClient.findDenomTrace(COSMOSHUB_LCD, "ibc/14F9BC3E44B8A9C1BE1FB08980FAB87034C9905EF17CF2F5008FC085218811CC");
+        DenomTraceDto receivedDto2 = denomTraceClient.findDenomTrace(OSMOSIS_LCD, "ibc/1DCC8A6CB5689018431323953344A9F6CC4D0BFB261E88C9F7777372C10CD076");
+        DenomTraceDto receivedDto3 = denomTraceClient.findDenomTrace(AKASHNET_LCD, "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2");
 
         DenomTraceDto dto1 = new DenomTraceDto("transfer/channel-141", "uosmo", true);
         DenomTraceDto dto2 = new DenomTraceDto("transfer/channel-8", "uregen", true);
-        DenomTraceDto dto3 = new DenomTraceDto("transfer/channel-0", "uatom", true);
+        DenomTraceDto dto3 = new DenomTraceDto("transfer/channel-0", "uatom",  true);
 
         assertAll(
-                () -> assertEquals(receivedDto1, dto1),
-                () -> assertEquals(receivedDto2, dto2),
-                () -> assertEquals(receivedDto3, dto3)
+                () -> assertEquals(receivedDto1.getBaseDenom(), dto1.getBaseDenom()),
+                () -> assertEquals(receivedDto1.getPath(), dto1.getPath()),
+                () -> assertEquals(receivedDto1.isSuccessReceived(), dto1.isSuccessReceived()),
+                () -> assertEquals(receivedDto2.getBaseDenom(), dto2.getBaseDenom()),
+                () -> assertEquals(receivedDto2.getPath(), dto2.getPath()),
+                () -> assertEquals(receivedDto2.isSuccessReceived(), dto2.isSuccessReceived()),
+                () -> assertEquals(receivedDto3.getBaseDenom(), dto3.getBaseDenom()),
+                () -> assertEquals(receivedDto3.getPath(), dto3.getPath()),
+                () -> assertEquals(receivedDto3.isSuccessReceived(), dto3.isSuccessReceived())
         );
     }
 
